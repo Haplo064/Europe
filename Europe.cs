6b8044b22c0282ef25914babdd6d271082daed99
Haplo064 <haplo064@gmail.com>
2020-12-20 21:51:37 +1030

    Keeping repo up to date
    -Added stuff

--- Europe.cs
+++ Europe.cs
@@ -23,12 +23,16 @@ namespace AccurateCountDown
         public bool start = true;
         public float scale = 2f;
         public bool debug = false;
+        public int pauseCheck = 0;
+        public float lastTime = 0;
+        public bool flipSwitch = true;
 
         static bool no_titlebar = true;
         static bool no_scrollbar = true;
         static bool no_move = false;
         static bool no_resize = false;
         static bool no_mouse = false;
+        static bool no_box = false;
 
 
         //void FUN_140298840(longlong param_1)
@@ -50,13 +54,6 @@ namespace AccurateCountDown
         public IntPtr stateFlags;
         public bool[] stateFlagsArray;
 
-        //__int64 __fastcall sub_140B2EB40(__int64 a1, __int64 a2)
-        [UnmanagedFunctionPointer(CallingConvention.ThisCall, CharSet = CharSet.Ansi)]
-        private delegate IntPtr UpdateStateFlags(Int64 arrayLocation, Int64 bitset);
-        private UpdateStateFlags updateStateFlags;
-        private Hook<UpdateStateFlags> updateStateFlagsHook;
-        public IntPtr updateStateFlagsPtr;
-
         public void Initialize(DalamudPluginInterface pluginInterface)
         {
             stateFlags = IntPtr.Zero;
@@ -65,18 +62,11 @@ namespace AccurateCountDown
             this.pluginInterface = pluginInterface;
             Configuration = pluginInterface.GetPluginConfig() as Config ?? new Config();
 
-            updateStateFlagsPtr = pluginInterface.TargetModuleScanner.ScanText("48 89 5C 24 ?? 48 89 6C 24 ?? 57 41 54 41 55 41 56 41 57 48 83 EC 20 4C 8B EA");
-            updateStateFlags = new UpdateStateFlags(updateStateFlagsFunc);
-            try
-            { updateStateFlagsHook = new Hook<UpdateStateFlags>(updateStateFlagsPtr, updateStateFlags, this); updateStateFlagsHook.Enable(); }
-            catch (Exception e)
-            { PluginLog.Log("BAD\n" + e.ToString()); }
-
             this.pluginInterface.UiBuilder.OnBuildUi += DrawWindow;
             this.pluginInterface.UiBuilder.OnOpenConfigUi += ConfigWindow;
             this.pluginInterface.CommandManager.AddHandler("/ctd", new CommandInfo(Command)
             {
-                HelpMessage = ""
+                HelpMessage = "Accurate Countdown config."
             });
 
             enabled = Configuration.Enabled;
@@ -87,10 +77,12 @@ namespace AccurateCountDown
             no_move = Configuration.No_Move;
             no_resize = Configuration.No_Resize;
             no_mouse = Configuration.No_Mouse;
+            no_box = Configuration.No_Box;
 
 
 
-            countdownPTR = pluginInterface.TargetModuleScanner.ScanText("??  89 ??  ??  ?? 57 48 83 ??  ?? 8B ??  ?? 48 8B ??  ?? 41 ?? 48 8B");
+            //countdownPTR = pluginInterface.TargetModuleScanner.ScanText("?? 89 ?? ?? ?? 57 48 83 ?? ?? 8B ?? ?? 48 8B ?? ?? 41 ?? 48 8B");
+            countdownPTR = pluginInterface.TargetModuleScanner.ScanText("48 89 5C 24 ?? 57 48 83 EC 40 8B 41");
             //funcPtr1 = pluginInterface.TargetModuleScanner.Module.BaseAddress + 0x298840;
             countdownTimer = new CountdownTimer(countdownTimerFunc);
             try
@@ -101,33 +93,9 @@ namespace AccurateCountDown
 
         }
 
-        public IntPtr updateStateFlagsFunc(Int64 arrayLocation, Int64 bitset)
-        {
-            stateFlags = (System.IntPtr)arrayLocation;
-            return updateStateFlagsHook.Original(arrayLocation, bitset);
-        }
-
-        public void UpdateStateFlagValues()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                if (stateFlags != IntPtr.Zero)
-                {
-                    if (Marshal.ReadByte(stateFlags + i) == 0)
-                    {
-                        stateFlagsArray[i] = false;
-                    }
-                    else
-                    {
-                        stateFlagsArray[i] = true;
-                    }
-                }
-            }
-        }
-
         public IntPtr countdownTimerFunc(ulong param_1)
         {
-
+            //PluginLog.Log(param_1.ToString());
             countDown = param_1;
             return countdownTimerHook.Original(param_1);
         }
@@ -141,8 +109,8 @@ namespace AccurateCountDown
         {
             this.pluginInterface.UiBuilder.OnBuildUi -= DrawWindow;
             this.pluginInterface.UiBuilder.OnOpenConfigUi -= ConfigWindow;
+            this.pluginInterface.CommandManager.RemoveHandler("/ctd");
             countdownTimerHook.Disable();
-            updateStateFlagsHook.Disable();
         }
 
         private void ConfigWindow(object Sender, EventArgs args)
@@ -152,14 +120,13 @@ namespace AccurateCountDown
 
         private void DrawWindow()
         {
-            UpdateStateFlagValues();
-
             ImGuiWindowFlags window_flags = 0;
             if (no_titlebar) window_flags |= ImGuiWindowFlags.NoTitleBar;
             if (no_scrollbar) window_flags |= ImGuiWindowFlags.NoScrollbar;
             if (no_move) window_flags |= ImGuiWindowFlags.NoMove;
             if (no_resize) window_flags |= ImGuiWindowFlags.NoResize;
             if (no_mouse) window_flags |= ImGuiWindowFlags.NoMouseInputs;
+            if (no_box) window_flags |= ImGuiWindowFlags.NoBackground;
 
             if (config)
             {
@@ -167,21 +134,23 @@ namespace AccurateCountDown
                 ImGui.Begin("Accurate Countdown Config", ref config);
                 ImGui.Checkbox("Enable Countdown", ref enabled);
                 ImGui.Checkbox("Enable EncounterTimer", ref enableEnc);
-                ImGui.Checkbox("Enable EncounterTimer for CD only", ref enableCDEnc);
+                ImGui.Checkbox(" - Only show EncounterTimer after a CountDown", ref enableCDEnc);
                 ImGui.Checkbox("No TitleBar", ref no_titlebar);
+                ImGui.Checkbox("No Background", ref no_box);
                 ImGui.Checkbox("No Moving", ref no_move);
                 ImGui.Checkbox("No Resizing", ref no_resize);
                 ImGui.Checkbox("Clickthrough", ref no_mouse);
                 ImGui.InputFloat("Font Scale", ref scale);
                 ImGui.Separator();
                 ImGui.Checkbox("Debug", ref debug);
+
                 if (no_titlebar) window_flags |= ImGuiWindowFlags.NoTitleBar;
                 window_flags |= ImGuiWindowFlags.NoScrollbar;
                 if (no_move) window_flags |= ImGuiWindowFlags.NoMove;
                 if (no_resize) window_flags |= ImGuiWindowFlags.NoResize;
                 window_flags |= ImGuiWindowFlags.NoCollapse;
                 if (no_mouse) window_flags |= ImGuiWindowFlags.NoMouseInputs;
-
+                if (no_box) window_flags |= ImGuiWindowFlags.NoBackground;
 
                 if (ImGui.Button("Save and Close Config"))
                 {
@@ -206,33 +175,38 @@ namespace AccurateCountDown
                 ImGui.End();
             }
 
-            if (enabled)
+            if (enabled && countDown !=0)
             {
-                if (countDown != 0)
+                if (Marshal.PtrToStructure<float>((IntPtr)countDown + 0x2c) == lastTime) { pauseCheck++; }
+                else { pauseCheck = 0; flipSwitch = true; }
+
+                if (pauseCheck > 50) { flipSwitch = false; }
+
+                if (Marshal.PtrToStructure<float>((IntPtr)countDown + 0x2c) > 0 && flipSwitch)
                 {
-                    if (Marshal.PtrToStructure<float>((IntPtr)countDown + 0x24) > 0)
-                    {
-                        ImGui.Begin("Countdown", ref enabled, window_flags);
-                        ImGui.SetWindowFontScale(scale);
-                        ImGui.Text(String.Format("{00:0.00}", Marshal.PtrToStructure<float>((IntPtr)countDown + 0x24)));
-                        ImGui.SetWindowFontScale(1f);
-                        ImGui.End();
-                        CDEnd = DateTime.Now;
-                    }
-                    if(Marshal.PtrToStructure<float>((IntPtr)countDown + 0x24) <= 0 && (DateTime.Now - CDEnd).TotalSeconds < 3)
-                    {
-                        ImGui.Begin("Countdown", ref enabled, window_flags);
-                        ImGui.SetWindowFontScale(scale);
-                        ImGui.Text("FIGHT");
-                        ImGui.SetWindowFontScale(1f);
-                        ImGui.End();
-                    }
-                }               
+                    ImGui.Begin("Countdown", ref enabled, window_flags);
+                    ImGui.SetWindowFontScale(scale);
+                    ImGui.Text(String.Format("{00:0.00}", Marshal.PtrToStructure<float>((IntPtr)countDown + 0x2c)));
+                    ImGui.SetWindowFontScale(1f);
+                    ImGui.End();
+                    CDEnd = DateTime.Now;
+                }
+
+                if(Marshal.PtrToStructure<float>((IntPtr)countDown + 0x2c) <= 0 && (DateTime.Now - CDEnd).TotalSeconds < 3 && flipSwitch)
+                {
+                    ImGui.Begin("Countdown", ref enabled, window_flags);
+                    ImGui.SetWindowFontScale(scale);
+                    ImGui.Text("FIGHT");
+                    ImGui.SetWindowFontScale(1f);
+                    ImGui.End();
+                }
+                lastTime = Marshal.PtrToStructure<float>((IntPtr)countDown + 0x2c);
             }
+
             if (enableEnc)
             {
 
-                if (stateFlagsArray[(int)StateFlag.combat])
+                if (pluginInterface.ClientState.Condition[Dalamud.Game.ClientState.ConditionFlag.InCombat])
                 {
                     if (start)
                     {
@@ -277,6 +251,7 @@ namespace AccurateCountDown
             Configuration.No_Move = no_move;
             Configuration.No_Resize = no_resize;
             Configuration.No_Mouse = no_mouse;
+            Configuration.No_Box = no_box;
             this.pluginInterface.SavePluginConfig(Configuration);
         }
     }
@@ -292,122 +267,8 @@ namespace AccurateCountDown
         public bool No_Move { get; set; } = false;
         public bool No_Resize { get; set; } = false;
         public bool No_Mouse { get; set; } = false;
+        public bool No_Box { get; set; } = false;
         public Num.Vector4 Colour { get; set; } = new Num.Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-
-    }
-
-    enum StateFlag
-    {
-        empty,
-
-        normal,
-        unconscious,
-        emote,
-        mounted,
-        crafting,
-        gathering,
-        melding,
-        sieging,
-        carrying,
-        mounted2,
-
-        badPosition,
-        racing,
-        miniGame,
-        verminion,
-        customMatch,
-        performing,
-        unknown,
-        unknown2,
-        unknown3,
-        unknown4,
-
-        unknown5,
-        unknown6,
-        unknown7,
-        unknown8,
-        occupied,
-        combat,
-        casting,
-        status,
-        status2,
-        occupied2,
-
-        occupied3,
-        occupied4,
-        occupied5,
-        duty,
-        occupied6,
-        dueling,
-        trading,
-        occupied7,
-        occupied8,
-        crafting2,
-
-        craftprep,
-        gathering2,
-        fishing,
-        unknown9,
-        betweenAreas,
-        stealthed,
-        unknown10,
-        jumping,
-        autoRunning,
-        occupied9,
-
-        betweenAreas2,
-        systemError,
-        loggingOut,
-        atLocation,
-        waitingForDuty,
-        boundByDuty,
-        time,
-        cutscene,
-        waitingDutyFinder,
-        creatingCharacter,
-
-        jumping2,
-        pvpDisplay,
-        status3,
-        mounting,
-        carrying2,
-        partyFinding,
-        housing,
-        transformed,
-        freeTrial,
-        beingMoved,
-
-        mounting3,
-        status4,
-        status5,
-        registeringMatch,
-        waitingMatch,
-        waitingTTMatch,
-        flying,
-        cutscene2,
-        deepDungeon,
-        swimming,
-
-        diving,
-        registeringTTMatch,
-        waitingTTMatch2,
-        cwp,
-        unknown11,
-        dutyRecord,
-        casting2,
-        state,
-        state2,
-        rolePlaying,
-
-        boundByDuty2,
-        readyingAnotherWorld,
-        waitingAnotherWorld,
-        parasol,
-        boundbyDuty3,
-        unknown12,
-        unknown13,
-        unknown14,
-        unknown15
 
     }
 }
